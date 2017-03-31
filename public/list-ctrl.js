832a4456e6e2ffd9ee273c1dfbac6f9e01f6414e
Edgar Polanco <theboss.edgar14@gmail.com>
2017-03-31 17:57:13 +0000

    vista en proceso

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -1,3 +1,8 @@
 angular.module("UniversityListApp").controller("ListCtrl", function($scope, $http){
-    
+    function refresh(){
+        $http.get("/api/v1/universities").then(function (response){
+            $scope.universities = response.data;
+        });
+    }
+    refresh();
 });
