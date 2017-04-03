5af83c1a8187b16d87380db32fb26b1c1ff41f6a
LuisTirado <ltiradocordova@gmail.com>
2017-04-03 20:31:33 +0000

    add travis configuration

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -1,46 +1,46 @@
-angular.module("UniversityListApp").controller("ListCtrl", function($scope, $http){
-    function refresh(){
-        $http.get("/api/v1/universities").then(function (response){
+angular.module("UniversityListApp").controller("ListCtrl", function($scope, $http) {
+    function refresh() {
+        $http.get("/api/v1/universities").then(function(response) {
             $scope.universities = response.data;
         });
-    }    
-    $scope.addUniversity = function (){
-        console.log("Adding university "+$scope.newUniversity);
-        $http.post("/api/v1/universities",$scope.newUniversity).then(function (){
+    }
+    $scope.addUniversity = function() {
+        console.log("Adding university " + $scope.newUniversity);
+        $http.post("/api/v1/universities", $scope.newUniversity).then(function() {
             refresh();
             $scope.newUniversity = [];
         });
-        
+
     };
-    $scope.delUniversity = function (acronym){
-        $http.delete("/api/v1/universities/"+acronym).then(function (){
+    $scope.delUniversity = function(acronym) {
+        $http.delete("/api/v1/universities/" + acronym).then(function() {
             refresh();
         });
-        
+
     };
-    $scope.delUniversities = function (){
-        $http.delete("/api/v1/universities").then(function (){
+    $scope.delUniversities = function() {
+        $http.delete("/api/v1/universities").then(function() {
             refresh();
         });
-        
+
     };
-    $scope.editUniversity1 = function (acronym){
-        $http.get("/api/v1/universities/"+acronym).then(function (response){
+    $scope.editUniversity1 = function(acronym) {
+        $http.get("/api/v1/universities/" + acronym).then(function(response) {
             $scope.newUniversity = response.data;
             $('#boton_add').attr("style", "display:none");
             $('#boton_edit').attr("style", "display:block");
         });
-        
+
     };
-    $scope.editUniversity2 = function (acronym){
-        $http.put("/api/v1/universities/"+acronym,$scope.newUniversity).then(function (){
+    $scope.editUniversity2 = function(acronym) {
+        $http.put("/api/v1/universities/" + acronym, $scope.newUniversity).then(function() {
             refresh();
             $('#boton_add').attr("style", "display:block");
             $('#boton_edit').attr("style", "display:none");
             $scope.newUniversity = [];
         });
-        
+
     };
-    
+
     refresh();
 });
