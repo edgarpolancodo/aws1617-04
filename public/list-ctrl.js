4935f5c3146e70ae42156b754da87b56f4115218
LuisTirado <ltiradocordova@gmail.com>
2017-04-04 20:17:11 +0000

    added new visual elements + get an university

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -1,37 +1,41 @@
 angular.module("UniversityListApp").controller("ListCtrl", function($scope, $http) {
+
+    $scope.a = "hola";
+
     function refresh() {
         $http.get("/api/v1/universities").then(function(response) {
             $scope.universities = response.data;
         });
     }
+
     $scope.addUniversity = function() {
         console.log("Adding university " + $scope.newUniversity);
         $http.post("/api/v1/universities", $scope.newUniversity).then(function() {
             refresh();
             $scope.newUniversity = [];
         });
-
     };
+
     $scope.delUniversity = function(acronym) {
         $http.delete("/api/v1/universities/" + acronym).then(function() {
             refresh();
         });
-
     };
+
     $scope.delUniversities = function() {
         $http.delete("/api/v1/universities").then(function() {
             refresh();
         });
-
     };
+
     $scope.editUniversity1 = function(acronym) {
         $http.get("/api/v1/universities/" + acronym).then(function(response) {
             $scope.newUniversity = response.data;
             $('#boton_add').attr("style", "display:none");
             $('#boton_edit').attr("style", "display:block");
         });
-
     };
+
     $scope.editUniversity2 = function(acronym) {
         $http.put("/api/v1/universities/" + acronym, $scope.newUniversity).then(function() {
             refresh();
@@ -39,7 +43,12 @@ angular.module("UniversityListApp").controller("ListCtrl", function($scope, $htt
             $('#boton_edit').attr("style", "display:none");
             $scope.newUniversity = [];
         });
+    };
 
+    $scope.searchUniversity = function(acronym) {
+        $http.get("/api/v1/universities/" + acronym).then(function(response) {
+            $scope.university = response.data;
+        });
     };
 
     refresh();
